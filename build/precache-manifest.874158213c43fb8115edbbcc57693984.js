1c43c0034b9ccf452b1baea15c9fdda591c85afa
Fawaz Adebayo <51902126+Pherwerz@users.noreply.github.com>
2020-07-19 09:36:39 +0100

    final commit

---
+++ build/precache-manifest.874158213c43fb8115edbbcc57693984.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef76e637570a1e953ad0ee19efc7d578",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c66ec308383b2885e7e",
+    "url": "/static/css/main.8ab945c6.chunk.css"
+  },
+  {
+    "revision": "4c9101d680451d5b7046",
+    "url": "/static/js/2.712a1be1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.712a1be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c66ec308383b2885e7e",
+    "url": "/static/js/main.1a932cf3.chunk.js"
+  },
+  {
+    "revision": "5132f4db0caab9ba715b",
+    "url": "/static/js/runtime-main.30538d72.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "2d6171f122009390bf1ff9180f4c6f89",
+    "url": "/static/media/icons.2d6171f1.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/static/media/logo.6387840f.svg"
+  },
+  {
+    "revision": "839f6d6c358c807bc21633ae4349ee28",
+    "url": "/static/media/logoWhite.839f6d6c.svg"
+  }
+]);
